e478488fe700d3aa05966e1d34415648c5feb4a8
michaelortega <michaelortega93@gmail.com>
2018-11-25 17:50:23 -0800

    d

--- scripter/settings.py
+++ scripter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2td#ypcnqt#g=ladj1uajt)#847)3ec5jz%oh2t7jv9nfy_=0n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scripter-project.herokuapp.com']
 
 
 # Application definition
